f77e33bab74014902a01ebba2d8edab7997fee73
shibe23 <shibe23.info@gmail.com>
2018-08-31 01:06:24 +0900

    Handling HTTP PUT Requests

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -17,18 +18,13 @@ app.get('/api/courses', (req, res)=> {
   res.send(courses);
 });
 
-app.get('/api/courses/:id', (req, res)=> {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given ID was not found')
-  res.send(course);
-});
-
 app.post('/api/courses', (req, res) => {
-   if (!req.body.name || req.body.name.length < 3) {
-     // 400 Bad Request
-     res.status(400).send('Name is required and should be minimum 3 chars');
-     return;
-   }
+  const {error} = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -36,6 +32,40 @@ app.post('/api/courses', (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+  // Look up the course
+  // If not existing, return 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found')
+
+  // Validate
+  // If invalid, return 400 - Bad request
+  const {error} = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  // Upadate course
+  course.name = req.body.name;
+  // Return the updated course
+  res.send(course);
+
+});
+
+app.get('/api/courses/:id', (req, res)=> {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found')
+  res.send(course);
+});
+
+function validateCourse(course){
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
