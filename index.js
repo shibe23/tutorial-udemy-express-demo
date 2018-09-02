7bc2c81a0a39f9eead7fb38639cdee09ee26ba49
shibe23 <shibe23.info@gmail.com>
2018-09-03 06:57:00 +0900

    Structuring Express Applications

--- index.js
+++ index.js
@@ -1,91 +1,31 @@
+const debug = require('debug')('app:startup');
+const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const Joi = require('joi');
-const logger = require('./logger');
+const logger = require('./middleware/logger');
+const courses = require('./routers/courses');
+const home = require('./routers/home');
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views'); // default
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(helmet());
+app.use('/api/courses', courses);
+app.use('/', home);
 
+// Configration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+debug(app.get('env'))
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  console.log('morgan enabled...');
-}
-
-const courses = [
-  { id: 1, name: 'course1'},
-  { id: 2, name: 'course2'},
-  { id: 3, name: 'course3'},
-]
-
-app.get('/', (req, res)=> {
-  res.send('Hello World!!');
-});
-
-app.get('/api/courses', (req, res)=> {
-  res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-  const {error} = validateCourse(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-  // Look up the course
-  // If not existing, return 404
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found')
-
-  // Validate
-  // If invalid, return 400 - Bad request
-  const {error} = validateCourse(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  // Upadate course
-  course.name = req.body.name;
-  // Return the updated course
-  res.send(course);
-
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found')
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
-})
-
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send('The course with the given ID was not found')
-  res.send(course);
-});
-
-
-
-function validateCourse(course){
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  return Joi.validate(course, schema);
+  debug('morgan enabled...'); // console.log()
 }
 
 const port = process.env.PORT || 3000;
