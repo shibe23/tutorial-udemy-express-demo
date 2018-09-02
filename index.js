373ab16a51b1417b2e56627a749ccbaf7b67c622
shibe23 <shibe23.info@gmail.com>
2018-09-02 18:09:25 +0900

    61. Environments

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const morgan = require('morgan');
+const helmet = require('helmet');
 const Joi = require('joi');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('morgan enabled...');
+}
 
 const courses = [
   { id: 1, name: 'course1'},
